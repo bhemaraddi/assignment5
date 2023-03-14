d3d4036ac892ed2f78f3f1e2cd93fe748ad9cc90
bhemaraddi <124993787+bhemaraddi@users.noreply.github.com>
2023-03-14 15:25:59 +0530

    Add files via upload

---
+++ assignment5.py
@@ -0,0 +1,111 @@
+#1challenge
+class var:
+    def __init__(self,x,y,z):
+        self.x=x
+        self.y=y
+        self.z=z
+    def  sum(self):
+       self.x=self.x*self.x
+       self.y=self.y*self.y
+       self.z=self.z*self.z
+       return self.x+ self.y+ self.z
+        
+result=var(1,3,5)   
+print(result.sum())
+
+#2challenge
+class calulator:
+    def __init__(self,num1,num2):
+        self.num1=num1
+        self.num2=num2
+    def add(self):
+        return self.num1+self.num2
+    def subtract(self):
+        if self. num1>self.num2:
+           return  self.num1-self.num2
+        else:
+             return self.num2-self.num1
+    def multiply(self):
+        return  self.num1*self.num2
+    def div(self):
+        if self. num1>self.num2:
+           return  self.num1/self.num2
+        else:
+           return  self.num2/self.num1
+result=calulator(10,94)  
+print(result.add())
+print(result.subtract())
+print(result.multiply())
+print(result.div())      
+
+#3challenge
+class student:
+    def setName(self,__name,key):
+        if key=="bheem":
+         self.__name=__name
+        else:
+           print("incorrect key")
+        
+    def getName(self):
+        if self.__name=="sachin":
+         print(" yes its crt name",self.__name)
+        else:
+            print("incorrect name!")
+    def setRollNumber(self,__rollnumber,key):
+        if key=="uttam":
+         self.__rollnumber=__rollnumber
+        else:
+           print("incorrect key")
+    def getRollNumber(self):
+        if self.__rollnumber==201:
+            print("yes its crt roll number",self.__rollnumber)
+        else:
+            print("incorrect rollnumber")    
+result=student()  
+result.setName("sachin","bheem")     
+result.setRollNumber(201,"uttam")
+result.getName()
+result.getRollNumber()
+
+#4challenge
+class account:
+    def __init__(self,title,balance):
+        self.title=title
+        self.balance=balance
+
+class Savaccount(account):
+    def __init__(self, title,balance,interest):
+        super().__init__(title, balance)
+        self.interest=interest
+        self.interest= self.balance*self.interest/100
+        print(self.interest)
+result=Savaccount("sachin",5000,5) 
+
+#5challenge
+class account:
+    def __init__(self,title,balance):
+        self.title=title
+        self.balance=balance
+    def deposit(self, d):
+        self.balance = self.balance + d
+        print(self.balance)
+    def Withdrawal(self , w): 
+        if(self.balance < w):
+          print("impossible operation! Insufficient balance !")
+        else:
+           self.balance = self.balance - w
+           print(self.balance)
+    def getBalance(self):
+       print(self.balance)
+       
+
+class Savaccount(account):
+    def __init__(self, title,balance,interest):
+        super().__init__(title, balance)
+        self.interest=interest
+        self.interest= self.balance*self.interest/100
+        print(self.interest)
+result=Savaccount("sachin",5000,5) 
+result.getBalance()
+result.deposit(200)
+result.Withdrawal(20)
